76ab79427400a987fe1959af52346b3b07dad143
Antanas Verbitskas <johnmnemonik.jm@gmail.com>
2018-07-24 08:59:39 +0300

    alpine:3.5

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,6 +6,11 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG
-docker push $TRAVIS_REPO_SLUG
-#
+docker tag 1915c636d784 kraus/cicd-buzz:latest
+docker login docker.io
+docker push kraus/cicd-buzz:latest
+#docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG
+#docker push $TRAVIS_REPO_SLUG
+#docker tag 1915c636d784 kraus/cicd-buzz:latest
+#docker login docker.io
+#docker push kraus/cicd-buzz:latest
